73d66ca9986bed2523571992a8166eaaf595a791
ahmed gamal <agemy844@gmail.com>
2021-05-04 18:03:35 +0200

    v 1.0.3 added generatedRoutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 //preparing to a real simple project represented in patch 1.0.3
 // the aim of this patch is use the bloc with perfect structure as could as possible
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:redux_project/buiseniss%20logic/cubit/counter_cubit.dart';
-import 'package:redux_project/presentation/Screens/SecondScreen.dart';
-import 'package:redux_project/presentation/Screens/firstScreen.dart';
-
-import 'presentation/Screens/ThirdScreen.dart';
+import 'package:redux_project/presentation/Router/genratedRoutes.dart';
 
 void main() {
   runApp(MyApplication());
@@ -22,24 +18,18 @@ class MyApplication extends StatefulWidget {
 
 class _MyApplicationState extends State<MyApplication> {
   CounterCubit _counterCubit = CounterCubit();
+  AppRouter appRouter;
+
+  @override
+  void initState() {
+    appRouter = AppRouter(counterCubit: _counterCubit);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        FirstScreen.PageRoute: (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: FirstScreen(),
-            ),
-        SecondScreen.PageRoute: (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: SecondScreen(),
-            ),
-        ThirdScreen.PageRoute: (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: ThirdScreen(),
-            ),
-      },
+      onGenerateRoute: appRouter.onGenerateRoutes,
     );
   }
 
